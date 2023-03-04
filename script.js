ca97e0b77d442f2ec859b1d9f747e25164a1e972
YassenSamuilov <121310799+YassenSamuilov@users.noreply.github.com>
2023-03-04 15:53:04 +0200

    Resizing the map and aligning the two dimensional array

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var mainBoard = document.getElementById('mainBoard'); // variable to access mainBoard class
-var marginMap = 11; //this is the margin in pixels, for the position of the cells, because there are walls at the top left corner so the cells cant start there
-var sizeX = 22;  // X of MapArray , how many cells will be there on every row
-var sizeY = 22; // Y of MapArray , how many cells will be there on every column
-var cellSize = 27; //size of the cells in pixels
+var marginMap = 1; //this is the margin in pixels, for the position of the cells, because there are walls at the top left corner so the cells cant start there
+var sizeX = 28;  // X of MapArray , how many cells will be there on every row
+var sizeY = 31; // Y of MapArray , how many cells will be there on every column
+var cellSizeX = 19.57; //size of the wight of cells in pixels         !! the map is not a square therefore the cells will not be the squares !!
+var cellSizeY = 19.35; //size of the height of cells in pixels
 var MapArray = [];  // array of cells, for the movement and position of the pacman charachter and the ghosts
-var direction = "right" //direction of pacman when a key is pressed
+var direction = "left" //direction of pacman when a key is pressed
 
 
 
@@ -49,7 +50,7 @@ movement(direction){
  // function that draws the pacman to its new location
   display(newX, newY) {
     MapArray[newX][newY].style.backgroundImage = "url('SpriteSheet.v1.png')";
-    MapArray[newX][newY].style.backgroundPosition = "-912px -2px";
+    MapArray[newX][newY].style.backgroundPosition = "-1105px -1px";
   
     this.x = newX;
     this.y = newY;
@@ -62,16 +63,18 @@ for (let x = 0; x < sizeY; x++) {
     for (let y = 0; y < sizeX; y++) {
         MapArray[x][y] = document.createElement("div"); //creating a cell
         
-        MapArray[x][y].style.height = `${cellSize}px`; //height of the cell
-        MapArray[x][y].style.width = `${cellSize}px`; //widht of the cell
-        MapArray[x][y].style.marginLeft = `${(y*cellSize)+marginMap}px` // y of a cell times the cell size we want plus the margin(because of the walls), so that cell 2,3... are next to cell 1
-        MapArray[x][y].style.marginTop = `${(x*cellSize)+marginMap-3}px`  //x of a cell times the cell size we want plus the margin(because of the walls) minus 5 because the map requires it,it will be under the desired height, so that cell 2,3... are under cell 1
+        MapArray[x][y].style.height = `${cellSizeY}px`; //height of the cell
+        MapArray[x][y].style.width = `${cellSizeX}px`; //widht of the cell
+        MapArray[x][y].style.marginLeft = `${(y*cellSizeX)+marginMap}px` // y of a cell times the X cell size we want plus the margin(because of the walls), so that cell 2,3... are next to cell 1
+        MapArray[x][y].style.marginTop = `${(x*cellSizeY)}px`  //x of a cell times the Y cell size we want 
         MapArray[x][y].style.borderStyle = "solid";  // borderis solid
         MapArray[x][y].style.borderWidth = "1px";  //border's widht 1px
         MapArray[x][y].style.padding = "0px";  //nopadding
         MapArray[x][y].style.position = "absolute"; //position is absolute
         MapArray[x][y].style.borderColor = "red";  //color of the border is red so that we can visualise them for now 
-
+          // if(x==0 && y==13 ||y==14 && x==0){
+          //   MapArray[x][y].style.backgroundColor = "red";
+          // }
         mainBoard.appendChild(MapArray[x][y]);  // creating the div
     }
 }
@@ -101,4 +104,4 @@ document.addEventListener("keydown", (event) => {
 //moves the character every one second by executing the movement function with the argument direction(in which direction the pacman will move)
 setInterval(function() {
   pacman.movement(direction);
-}, 1000);
+}, 100);
